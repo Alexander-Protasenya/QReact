7bcee5bff6eadffa0ce20c895a3b4ee803fffef6
Alexander Protasenya <alex.p84x@gmail.com>
2025-06-12 23:34:11 +0200

    Update bundle configuration

--- webpack.build.js
+++ webpack.build.js
@@ -1,19 +1,18 @@
 const path = require('path');
 
-module.exports = [
-	{
-		// TODO: Better result of minification: https://minify-js.com/ (ecma2016, module)
-		entry: './src/QReact.mjs',
-		output: {
-			filename: 'QReact.min.mjs',
-			path: path.resolve(__dirname, 'publish'),
-			library: {
-				type: 'module',
-			},
-			clean: true,
+module.exports = {
+	// TODO: Better result of minification: https://minify-js.com/ (ecma2016, module)
+	target: ['web', 'es2020'],
+	entry: './src/QReact.mjs',
+	output: {
+		filename: 'QReact.min.mjs',
+		path: path.resolve(__dirname, 'publish'),
+		library: {
+			type: 'module',
 		},
-		experiments: {
-			outputModule: true,
-		},
+		clean: true,
 	},
-];
+	experiments: {
+		outputModule: true,
+	},
+};
